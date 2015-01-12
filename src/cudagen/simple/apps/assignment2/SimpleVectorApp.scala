d351247c9958d006e30de12650814895f8dfb5f3
Alexander Samoilov <samoilov.alexander@huawei.com>
2015-01-12 18:46:07 +0300

    an attempt to execute: call embeddedMain().

--- src/cudagen/simple/apps/assignment2/SimpleVectorApp.scala
+++ src/cudagen/simple/apps/assignment2/SimpleVectorApp.scala
@@ -1,8 +1,17 @@
 package ppl.apps.assignment2
 
 import ppl.dsl.assignment2.{SimpleVectorApplicationRunner, SimpleVectorApplication}
+import ppl.delite.runtime._
 
-object SimpleVectorAppRunner extends SimpleVectorApplicationRunner with SimpleVectorApp
+object SimpleVectorAppRunner extends SimpleVectorApplicationRunner with SimpleVectorApp {
+  reifyEffects {
+    //args = Array("out.deg")
+    main()
+    //execute(scala.Array("out.deg"))
+    Config.numCuda = 1
+    Delite.embeddedMain(scala.Array("out.deg", "42"), staticDataMap)
+  }
+}
 
 trait SimpleVectorApp extends SimpleVectorApplication {
 
